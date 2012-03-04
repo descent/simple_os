a77fdf47ec4859eb663dea0fbfdb93705261d3b7
descent <descent0@gmail.com>
2012-03-04 15:49:29 +0800

    modified:   start.c     test itoa(), bss variable.

--- start.c
+++ start.c
@@ -1,4 +1,8 @@
 #include "type.h"
+#include "protect.h"
+
+u8 gdt_ptr[6];
+Descriptor gdt[GDT_SIZE];
 
 void s32_put_char(u8 ch, u8 *vb)
 {
@@ -38,6 +42,7 @@ char* s32_itoa(int n, char* str, int radix)
   while(n)
   {
     *p++=digit[n%radix];
+    //s32_put_char(*(p-1), (u8*)(0xb8000+80*2));
     n/=radix;
   }
   *p=0;
@@ -53,11 +58,52 @@ char* s32_itoa(int n, char* str, int radix)
   return str;
 }
 
+void p_asm_memcpy(void *dest, void *src, u16 n);
+
+void init_protected_mode_by_c()
+{
+  p_asm_memcpy(&gdt, (void*)(*((u32*)(&gdt_ptr[2]))), *((u16*)(&gdt_ptr[0])) + 1);
+
+  u16 *p_gdt_limit = (u16*)(&gdt_ptr[0]);
+  u32 *p_gdt_base = (u32*)(&gdt_ptr[2]);
+  *p_gdt_limit = GDT_SIZE * sizeof(Descriptor) - 1;
+  *p_gdt_base = (u32)&gdt_ptr;
+}
+
+void ptr_test(u8 *ch)
+{
+  ch[0] = 'X';
+  //*ch = 'X';
+}
+
+int AA;
+
 void startc()
 {
   u8 *ro_str="ro_string";
   u8 *vb=(u8*)0xb8000;
+  int i=65536;
+  int c=AA+1;
+  u8 stack_str[10]="y";
+  u8 *sp = stack_str;
 
   *vb = 'W';
-  s32_print(ro_str, vb);
+  #if 0
+  while(1);
+  #else
+  __asm__ ("nop\n\t");
+  __asm__ ("nop\n\t");
+  #endif
+  #if 0
+  u8 ch='Q';
+  *(vb+15*2) = ch;
+  ptr_test(&ch);
+  *(vb+17*2) = ch;
+  #endif
+  //s32_print(ro_str, vb+80*2);
+  sp = s32_itoa(i, stack_str, 10);
+  s32_print(sp, (u8*)(0xb8000+160));
+  //s32_put_char(sp[0], vb);
+  sp = s32_itoa(c, stack_str, 10);
+  s32_print(sp, (u8*)(0xb8000+160*2));
 }
