eba7bb6cdc979aaea26d01bcae44d8bfaa5ebd4e
descent <descent0@gmail.com>
2012-03-29 15:24:09 +0800

    modified:   clock.c     use k_reenter to check if reenter.

--- clock.c
+++ clock.c
@@ -7,12 +7,19 @@ void s32_print(const u8 *s, u8 *vb);
 void clear_line(u8 line_no);
 void loop_delay(int time);
 
+extern int k_reenter;
 
 void clock_handler(int irq)
 {
   static u16 p = 4;
   s32_print("@", (u8*)(0xb8000+p*2));
   ++p;
+  if (k_reenter != 0)
+  {
+    s32_print("!", (u8*)(0xb8000+p*2));
+    return;
+  }
+
 
   ++ready_process;
   if (ready_process >= &proc_table[NR_TASKS])
