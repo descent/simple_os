2c8e3b20497d9039d24e5a3eec9f7676b4b8d3f1
descent <descent0@gmail.com>
2012-04-16 18:19:52 +0800

    modified:   process.c modified:   process.h     change process number to 1, and call system call get_ticks() in proc_a() .

--- process.c
+++ process.c
@@ -58,12 +58,18 @@ void proc_a(void)
 
   int i = 0;
   
+  int sys_get_ticks(void);
+  int get_ticks(void);
+
   while(1)
   {
+    //int r = sys_get_ticks();
+    int r = get_ticks();
     s32_print("A", cur_vb);
     s32_print_int(i++, cur_vb, 10);
     s32_print(".", cur_vb);
-    loop_delay(100);
+    s32_print_int(r, cur_vb, 10);
+    loop_delay(1);
   }
 
 }
@@ -135,8 +141,8 @@ void proc_c(void)
 
 Task tasks[NR_TASKS] = {
                          {proc_a, TASK_STACK, "proc a"},
-                         {proc_b, TASK_STACK, "proc b"},
-                         {proc_c, TASK_STACK, "proc c"},
+//                         {proc_b, TASK_STACK, "proc b"},
+//                         {proc_c, TASK_STACK, "proc c"},
                        };
 
 void init_proc(void)
