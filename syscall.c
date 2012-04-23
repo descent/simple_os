c5e4d0b18016546b20c03982319f25b2039f8de7
descent <descent0@gmail.com>
2012-04-23 20:45:13 +0800

    modified:   syscall.c   sys_get_ticks() - directly return ticks .

--- syscall.c
+++ syscall.c
@@ -9,13 +9,18 @@ void s32_print_int(int i, u8 *vb, int radix);
 void clear_line(u8 line_no);
 void loop_delay(int time);
 
+
 int sys_get_ticks(void)
 {
+  extern u8 ticks;
+
+#if 0
   static int p=0;
   s32_print("+", (u8*)(0xb8000+p));
   p += 2;
   p = (p % 160);
-  return 9;
+#endif
+  return ticks;
 }
 
 SystemCall sys_call_table[NR_SYS_CALL] = {sys_get_ticks};
