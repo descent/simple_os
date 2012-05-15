a27fb9fef6ea015315fb24aa5969a751688ea64c
descent <descent0@gmail.com>
2012-05-15 22:20:01 +0800

    modified:   cb.c     only test, the code should run right, but can work in qemu.

--- c-bootloader/cb.c
+++ c-bootloader/cb.c
@@ -4,11 +4,19 @@ void print(const char   *s);
 
 void main(void)
 {
+#if 1
+  unsigned char *vb = (unsigned char *)0xb8000;
+  *vb = 'A';
+  *(unsigned char *)0xb8001 = 0xc;
+  *(unsigned char *)0xb8002 = 'B';
+  *(unsigned char *)0xb8003 = 0xc;
+#else
   print("hello world");
+#endif
   while(1);
 }
 
-
+#if 0
 void print(const char   *s)
 {
   while(*s)
@@ -17,5 +25,5 @@ void print(const char   *s)
     s++;
   }
 }
-
+#endif
 
