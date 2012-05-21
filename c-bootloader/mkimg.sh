6690fa7c522d53d6b93e6059086f263f0bbf0c8e
descent <descent@weiya.com.tw>
2012-05-21 10:31:38 +0800

    modified:   mkimg.sh     if floppy doesn't exist, create it first.

--- c-bootloader/mkimg.sh
+++ c-bootloader/mkimg.sh
@@ -4,4 +4,19 @@ if [ "$1" == "" ] ; then
   exit -1
 fi
 
-dd if=$1 of=floppy.img bs=1 count=512 conv=notrunc
+FN=./floppy.img
+if [ -f $FN ]
+then
+    echo "$FN exists"
+else
+  echo "$FN doesn't exist, create $FN"
+  dd if=/dev/zero of=floppy.img skip=1 seek=1 bs=512 count=2879
+fi
+
+echo ""
+
+echo "write $1 to $FN"
+dd if=$1 of=$FN bs=1 count=512 conv=notrunc
+
+
+
