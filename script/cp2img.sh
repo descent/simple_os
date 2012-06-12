df71d3d72b40be892ad6f9a4ff06c9886b62b6ff
descent <descent0@gmail.com>
2012-06-12 17:33:03 +0800

    little fixed

--- script/cp2img.sh
+++ script/cp2img.sh
@@ -2,7 +2,7 @@
 # copy needed files to floppy image.
 
 #make c_init_by_boot_loader.bin
-make kloaderp.bin
+#make kloaderp.bin
 make p_kernel.elf
 sudo  mount -o loop boot.img /mnt/
 sudo  cp kloader.bin  /mnt/loader.bin
@@ -10,7 +10,8 @@ sudo  cp kernel_loader/kloaderp.bin  /mnt/loader.bin
 #sudo  cp kloader.bin  /mnt/test.bin
 sudo  cp kernel.bin  /mnt/
 sudo  cp kernel.elf  /mnt/
-sudo  cp p_kernel.elf  /mnt/
+sudo  cp -v p_kernel.elf  /mnt/
+#sudo  cp rom.fs  /mnt/
 sudo  cp /home/descent/git/write_os/src/chapter3/1.5/idt.com /mnt/
 sudo  cp /home/descent/git/write_os/src/chapter3/1.5/idt.com.elf /mnt/idt.elf
 #sudo  cp c_init_by_boot_loader.bin  /mnt/loader.bin
