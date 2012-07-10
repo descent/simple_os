19a162bc293f6d36ee6be72ccc12088af187b660
descent <descent@weiya.com.tw>
2012-07-10 15:21:59 +0800

    add get_next_16_boundary(). fixed union definition.

--- include/romfs.h
+++ include/romfs.h
@@ -5,17 +5,28 @@ typedef struct RomFsHeader_
 {
   union
   {
+    u8 id_str[8];
     struct
     {
-      u8 str[8];
-    }id_str;
-    u32 nextfh;
-    u32 spec;
+      u32 nextfh;
+      u32 spec;
+    }header8;
   }u;
   u32 size;
   u32 checksum;
 }RomFsHeader;
 
+static inline u32 get_next_16_boundary(int cur_offset)
+{
+  u32 len = cur_offset;
+
+  if (len&15)
+  {
+    len += 16-(len&15);
+  }
+  //len+=16; // next 16 boundary
+  return len;
+}
 
 
 #endif
