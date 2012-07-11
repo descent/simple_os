fb41fa147aa9798bd159c5fa3760d931184ea744
descent <descent0@gmail.com>
2012-07-11 21:59:25 +0800

    add s_strcmp()

--- include/k_string.h
+++ include/k_string.h
@@ -13,5 +13,14 @@ static inline u32 s_strlen(const char *s)
   return len;
 }
 
+static inline int s_strcmp(const char *s1, const char *s2)
+{
+  s8 res;
+  while(1)
+    if ((res = *s1 - *s2++) != 0 || !*s1++)
+      break;
+  return res;
+}
+
 
 #endif
