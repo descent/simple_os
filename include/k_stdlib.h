a278e1e389f34a9f6014e43aee05a87794789ea3
descent <descent@weiya.com.tw>
2012-07-13 10:38:31 +0800

    new file:   k_stdlib.h

---
+++ include/k_stdlib.h
@@ -0,0 +1,10 @@
+#ifndef K_STDLIB_H
+#define K_STDLIB_H
+
+// sign version
+char* s32_itoa_s(int n, char* str, int radix);
+
+// unsign version
+char* s32_itoa(u32 n, char* str, int radix);
+
+#endif
