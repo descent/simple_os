97ca3ebe887441d545c0607d5799391fc81ffc4a
descent <descent@weiya.com.tw>
2012-07-19 14:28:07 +0800

    move load elf to here

--- kernel_loader/protected_code.c
+++ kernel_loader/protected_code.c
@@ -1,5 +1,9 @@
 // test protected mode
 #include "type.h"
+#include "kl_global.h"
+#include "elf.h"
+#include "k_stdio.h"
+#include "k_string.h"
 
 int AA;
 
@@ -24,4 +28,67 @@ void jump_kernel()
 {
   __asm__ ("jmp $0x8, $0x5000\t\n");
   __asm__ ("jmp 0x5000\t\n");
+}
+
+void s32_memcpy(u8 *dest, const u8 *src, u32 n)
+{
+  for (int i=0; i < n ; ++i)
+    *dest++ = *src++;
+}
+
+// in this state, cannot use global, static, bss data,
+// because this address is base on 0x100, but kloaderp.bin is loaded to 0x9000:0x0100,
+// so need added 0x9000X16 is global, static, bss data real address.
+// I only use stack for variable.
+// copy_elf_code() is run under protocted mode.
+void copy_elf_code(void)
+{
+  u8 *vb = (u8 *)0xb8002;
+  u8 *attr = (u8 *)0xb8003;
+
+  *((u8 *)0xb8000) = 'P';
+  *vb = 'M';
+  *attr = 9;
+  // copy kernel to proper position by elf information
+
+
+  u8 *buff = (KERNEL_ES*16)+(u8*)LOAD_KERNEL_OFFSET;
+  Elf32Ehdr *elf_header = (Elf32Ehdr*)buff;
+  Elf32Phdr *elf_pheader = (Elf32Phdr*)((u8 *)buff + elf_header->e_phoff);
+
+  int line=0;
+  clear_line(line);
+  s32_print_int(elf_header->e_entry, (u8*)(0xb8000+160*line), 16);
+
+
+#if 0
+  print("\r\nelf_header->e_entry: ");
+  s16_print_int(elf_header->e_entry, 10);
+
+  print("\r\nelf_header->e_phnum: ");
+  s16_print_int(elf_header->e_phnum, 10);
+#endif
+
+  for (int i=0 ; i < elf_header->e_phnum; ++i)
+  {
+    if (CHECK_PT_TYPE_LOAD(elf_pheader))
+    {
+      //print_num(elf_pheader->p_vaddr, "elf_pheader->p_vaddr");
+      ++line;
+      clear_line(line);
+      s32_print_int(elf_pheader->p_vaddr, (u8*)(0xb8000+160*line), 16);
+
+      ++line;
+      clear_line(line);
+      s32_print_int(elf_pheader->p_offset, (u8*)(0xb8000+160*line), 16);
+      //print_num(elf_pheader->p_offset, "elf_pheader->p_offset");
+      //print_num(elf_pheader->p_filesz, "elf_pheader->p_filesz");
+      //asm_absolute_memcpy((u8*)elf_pheader->p_vaddr, buff+(elf_pheader->p_offset), elf_pheader->p_filesz);
+      s32_memcpy((u8*)elf_pheader->p_vaddr, buff+(elf_pheader->p_offset), elf_pheader->p_filesz);
+    }
+    ++elf_pheader;
+  }
+  //while(1);
+  BOCHS_MB
+  goto *elf_header->e_entry;
 }
