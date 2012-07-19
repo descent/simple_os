6fd3000767953f5373a978c07b6f96b1f397e6d5
descent <descent@weiya.com.tw>
2012-07-19 14:27:30 +0800

    move printXX to io/k_stdio.c

--- start.c
+++ start.c
@@ -7,6 +7,7 @@
 #include "storage.h"
 #include "romfs.h"
 #include "k_string.h"
+#include "k_stdio.h"
 #include "endian.h"
 #include "vfs.h"
 
@@ -42,24 +43,10 @@ Descriptor gdt[GDT_SIZE];
 u8 idt_ptr[6];
 Gate idt[IDT_SIZE];
 
-u8 *cur_vb = (u8*)0xb8000+160;
 
 IrqHandler irq_table[NR_IRQ];
 
 
-// char fg attribute 
-#define HRED 0xc
-#define HGREEN 0xa
-#define HBLUE 0x9
-#define HWHITE 0xf
-
-#define RED 4
-#define GREEN 2
-#define BLUE 1
-#define WHITE 7
-
-#define EFLAGS_AC_BIT           0x00040000
-#define CR0_CACHE_DISABLE       0x60000000
 
 u32 io_load_eflags(void);
 void io_store_eflags(u32 eflg);
@@ -111,31 +98,6 @@ void h2c(u8 hex, u8 ch[2])
   }
 }
 
-void dump_u8(u8 *buff, int len)
-{
-  void h2c(u8 hex, u8 ch[2]);
-
-  u8 *vb = (u8*)0xb8000;
-  int line=24;
-
-    for (int i=0 ; i < len ; ++i)
-    {
-      u8 c[4]="";
-      //if (i%16==0)
-      //  s32_print("\r\n", vb);
-      u8 h=*(buff+i);
-      c[3]=0;
-      c[2]=0x20; // space
-      h2c(h, c);
-      s32_print(c, vb);
-      vb+=6;
-    }
-    #if 0
-    clear_line(line);
-    s32_print("len:", (u8*)(0xb8000+160*line));
-    s32_print_int(len, (u8*)(0xb8000+160*line+12*2), 16);
-    #endif
-}
 
 // copy from 30days_os/projects/09_day/harib06b/bootpack.c
 u32 memtest(volatile u32 start, volatile u32 end)
@@ -197,141 +159,7 @@ not_memory:
 }
 #endif
 
-void clear_line(u8 line_no)
-{
-  u8* vb = (u8*)0xb8000 + 160*line_no;
 
-  for (int x = 0; x < 80; ++x)
-  {
-    *vb++ = 0x20;
-    *vb++ = WHITE;
-  }
-}
-
-void clear()
-{
-  u8* vb = (u8*)0xb8000;
-  const u8 row=25;
-  
-  for (int x = 0; x < 80*row ; ++x)
-  {
-    *vb++ = 0x20;
-    *vb++ = WHITE;
-  }
-}
-
-// prefix s32 means simple, 32bit code (in x86 protected mode)
-void s32_put_char(u8 ch, u8 *vb)
-{
-  *vb = ch;
-}
-
-// text mode screen 25 * 80
-void s32_print(const u8 *s, u8 *vb)
-{
-  while(*s)
-  {
-    s32_put_char(*s, vb);
-    ++s;
-    vb+=2;
-  }
-  cur_vb = vb;
-  if ((int)cur_vb >= 0xb8000+160*24)
-    cur_vb = (u8*)0xb8000+160;
-}
-
-// sign version
-char* s32_itoa_s(int n, char* str, int radix)
-{
-  char digit[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char* p=str;
-  char* head=str;
-  //int radix = 10;
-
-//  if(!p || radix < 2 || radix > 36)
-//    return p;
-  if (n==0)
-  {
-    *p++='0';
-    *p=0;
-    return str;
-  }
-  if (radix == 10 && n < 0)
-  {
-    *p++='-';
-    n= -n;
-  }
-  while(n)
-  {
-    *p++=digit[n%radix];
-    //s32_put_char(*(p-1), (u8*)(0xb8000+80*2));
-    n/=radix;
-  }
-  *p=0;
-  #if 1
-  for (--p; head < p ; ++head, --p)
-  {
-    char temp=*head;
-    if (*(p-1) != '-')
-    {
-      *head=*p;
-      *p=temp;
-    }
-  }
-  #endif
-  return str;
-}
-
-// no sign version
-char* s32_itoa(u32 n, char* str, int radix)
-{
-  char digit[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char* p=str;
-  char* head=str;
-  //int radix = 10;
-
-//  if(!p || radix < 2 || radix > 36)
-//    return p;
-  if (n==0)
-  {
-    *p++='0';
-    *p=0;
-    return str;
-  }
-  if (radix == 10 && n < 0)
-  {
-    *p++='-';
-    n= -n;
-  }
-  while(n)
-  {
-    *p++=digit[n%radix];
-    //s32_put_char(*(p-1), (u8*)(0xb8000+80*2));
-    n/=radix;
-  }
-  *p=0;
-  #if 1
-  for (--p; head < p ; ++head, --p)
-  {
-    char temp=*head;
-    if (*(p-1) != '-')
-    {
-      *head=*p;
-      *p=temp;
-    }
-  }
-  #endif
-  return str;
-}
-
-void s32_print_int(int i, u8 *vb, int radix)
-{
-  u8 str[12]="";
-  u8 *str_ptr = str;
-
-  str_ptr = s32_itoa(i, str_ptr, radix);
-  s32_print(str_ptr, vb);
-}
 
 void p_asm_memcpy(void *dest, void *src, u16 n);
 void p_asm_memset(void *dest, int c, u16 n);
@@ -819,7 +647,7 @@ void test_romfs(void)
   //fs_type[ROMFS]->device->dout(fs_type[ROMFS]->device, buf, fs_type[ROMFS]->get_daddr(inode), inode->dsize);
 
   romfs_init();
-  s32_print("romfs init", (u8*)(0xb8000+160*24));
+  s32_print("romfs init xx", (u8*)(0xb8000+160*24));
   while(1);
   inode = fs_type[ROMFS]->namei(fs_type[ROMFS], "t1"); // get super block infomation
 
@@ -895,7 +723,7 @@ void load_init_boot(InitFunc *init_func)
   }
   u8 buf[512];
   storage[RAMDISK]->dout(storage[RAMDISK], buf, 0, sizeof(buf));
-  dump_u8(buf, 32);
+  p_dump_u8(buf, 32);
 
   test_romfs();
   //test_vga();
