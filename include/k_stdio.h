36aaa23f5d27e3ec339810832f7eb8a3d8682d41
descent <descent@weiya.com.tw>
2012-07-19 14:31:51 +0800

    add more function prototype

--- include/k_stdio.h
+++ include/k_stdio.h
@@ -1,6 +1,24 @@
 #ifndef K_STDIO_H
 #define K_STDIO_H
 
+#include "type.h"
+
+extern u8 *cur_vb;
+
+// char fg attribute 
+#define HRED 0xc
+#define HGREEN 0xa
+#define HBLUE 0x9
+#define HWHITE 0xf
+
+#define RED 4
+#define GREEN 2
+#define BLUE 1
+#define WHITE 7
+
+#define EFLAGS_AC_BIT           0x00040000
+#define CR0_CACHE_DISABLE       0x60000000
+
 // for kernel_loader.c
 // prefix s16 means Simple 16 bit code
 void s16_print_int(int i, int radix);
@@ -13,8 +31,9 @@ void s32_print(const u8 *s, u8 *vb);
 void clear_line(u8 line_no);
 void clear();
 
-void dump_u8(u8 *buff, int len);
+void p_dump_u8(u8 *buff, int len);
 void h2c(u8 hex, u8 ch[2]);
+void print_num(int n, u8 *sy);
 
 //bios int 16-0, Wait for Keypress and Read Character
 //so all previous bios print messages may be seen
@@ -25,12 +44,13 @@ static inline void bios_wait_key()
 }
 
 // reset disk
-void bios_reset_disk()
+static inline void bios_reset_disk()
 {
   __asm__ __volatile__("mov $0, %ah\n");
   __asm__ __volatile__("mov 0x0, %dl\n");
   __asm__ __volatile__("int $0x13\n");
 }
+
 
 // bochs magic break point
 #define BOCHS_MB __asm__ __volatile__("xchg %bx, %bx");
