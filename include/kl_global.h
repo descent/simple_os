ce5dcd09042cf7e0fc5b69d9577df8e8da027c5b
descent <descent@weiya.com.tw>
2012-07-19 14:26:27 +0800

    new file:   include/kl_global.h     for kernel loader macro.

---
+++ include/kl_global.h
@@ -0,0 +1,17 @@
+//kl meads kernel loader
+#ifndef KL_GLOBAL_H
+#define KL_GLOBAL_H
+
+#define KERNEL_ES 0x3000
+#define RAMDISK_ES 0x1000
+#define IMAGE_SIZE  8192
+#define BLOCK_SIZE  512
+#define READ_FAT_ADDR (0x3000) // original is 0x2000, but will overwrite bss (variable bpb), so change to 0x3000
+#define IMAGE_LMA   (0x4000)
+#define LOAD_KERNEL_OFFSET (0x0)
+//#define IMAGE_LMA   0x8000
+#define IMAGE_ENTRY 0x800c
+#define buf_addr_val (*(u8 volatile*(IMAGE_LMA)))
+
+#endif
+
