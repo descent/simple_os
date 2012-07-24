f0e73c901008328ee07cfd2cbfea96178398f01f
descent <descent@weiya.com.tw>
2012-07-24 15:07:22 +0800

    read scan code

--- tty/keyboard.c
+++ tty/keyboard.c
@@ -1,3 +1,4 @@
+#include "io.h"
 #include "k_stdio.h"
 
 #define KEYBOARD_IRQ 1
@@ -19,8 +20,11 @@ void init_keyboard(void)
 
 void keyboard_handler(int irq)
 {
-  clear_line(4);
-  s32_print("*", (u8*)(0xb8000+160*22));
+  static cur_pos=0;
+  //clear_line(22);
+  //s32_print("*", (u8*)(0xb8000+160*22));
 
-
+  u8 scan_code = io_in8(0x60);
+  s32_print_int(scan_code, (u8*)(0xb8000+160*22 + cur_pos*2), 16);
+  cur_pos+=3;
 }
