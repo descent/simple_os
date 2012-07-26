71c1e1d839f6dbfcdc4dddac2df4c74f6ea70e86
descent <descent@weiya.com.tw>
2012-07-26 13:34:04 +0800

    parse scancode

--- tty/keyboard.c
+++ tty/keyboard.c
@@ -52,3 +52,152 @@ u8 get_byte_from_kb_buf(void)
   enable_int();
   return scan_code;
 }
+
+u32 keymap[] =
+{
+0,
+KEY_ESC,
+'1',
+'2',
+'3',
+'4',
+'5',
+'6',
+'7',
+'8',
+'9',
+'0',
+'-',
+'=',
+KEY_BACKSPACE,
+KEY_TAB,
+'q',
+'w',
+'e',
+'r',
+'t',
+'y',
+'u',
+'i',
+'o',
+'p',
+'[',
+']',
+KEY_ENTER,
+KEY_CTRL_L,
+'a',
+'s',
+'d',
+'f',
+'g',
+'h',
+'j',
+'k',
+'l',
+';',
+'\'',
+'`',
+KEY_SHIFT_L,
+'\\',
+'z',
+'x',
+'c',
+'v',
+'b',
+'n',
+'m',
+',',
+'.',
+'/',
+KEY_SHIFT_R,
+'*',
+KEY_CAPSLOCK,
+KEY_F1,
+KEY_F2,
+KEY_F3,
+KEY_F4,
+KEY_F5,
+KEY_F6,
+KEY_F7,
+KEY_F8,
+KEY_F9,
+KEY_F10,
+KEY_NUMLOCK,
+KEY_NUMLOCK,
+KEY_NUMLOCK,
+KEY_NUMLOCK,
+KEY_NUMLOCK,
+KEY_UP,
+/* 0x49 - PgUp          */      KEY_NUMLOCK,
+/* 0x4A - '-'           */      KEY_NUMLOCK,
+/* 0x4B - Left          */      KEY_NUMLOCK,
+/* 0x4C - MID           */      KEY_NUMLOCK,
+/* 0x4D - Right         */      KEY_NUMLOCK,
+/* 0x4E - '+'           */      KEY_NUMLOCK,
+/* 0x4F - End           */      KEY_NUMLOCK,
+/* 0x50 - Down          */      KEY_DOWN,
+'q',
+'w',
+'q',
+'q',
+'w',
+'q',
+'w',
+'q',
+'q',
+'q',
+'w',
+'q',
+'q',
+'w',
+'q',
+'w',
+'q',
+'q',
+'q',
+'w',
+'q',
+'q',
+'w',
+'q',
+'q',
+'w',
+'w',
+};
+
+int lookup_key(u8 scan_code)
+{
+  return keymap[scan_code];
+  //return KEY_UP;
+}
+
+int parse_scan_code(KeyStatus *key_status)
+{
+  u8 make;
+  int ret=0;
+
+  if(kb_buf.count > 0)
+  {
+    u8 scan_code = get_byte_from_kb_buf();
+    switch (scan_code)
+    {
+      case 0xe1:
+        break;
+      case 0xe0:
+        scan_code = get_byte_from_kb_buf();
+        key_status->key = lookup_key(scan_code & 0x7f);
+        key_status->press = ((scan_code & 0x80) ? RELEASE: PRESS);
+        break;
+      default:
+        key_status->key = lookup_key(scan_code & 0x7f);
+        key_status->press = ((scan_code & 0x80) ? RELEASE: PRESS);
+        break;
+    }
+
+  }
+  else
+  {
+    ret=1;
+  }
+  return ret;
+}
