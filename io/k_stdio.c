377e459d4123b29a7293ecd3049e2ea4dbcffb48
descent <descent@weiya.com.tw>
2012-07-30 09:36:11 +0800

    modified:   io/k_stdio.c modified:   tty/console.c   move s32_print_char() from io/k_stdio.c to tty/console.c

--- io/k_stdio.c
+++ io/k_stdio.c
@@ -2,43 +2,8 @@
 #include "k_stdio.h"
 #include "console.h"
 
-u8 *text_vb = (u8*)0xb8000;
-u8 text_fg;
-u8 text_bg;
-u16 cur_pos=0;
 u8 *cur_vb = (u8*)0xb8000+160;
 
-// prefix s32 means simple, 32bit code (in x86 protected mode)
-void s32_print_char(u8 ch)
-{
-  if (ch=='\r')
-  {
-    text_vb = (u8*)((u32)text_vb - (((u32)text_vb-0xb8000) % 160));
-    cur_pos = (((u32)text_vb - 0xb8000)/2);
-    set_cursor(cur_pos);
-    s32_print_int(cur_pos, (u8*)(0xb8000+160*1 + 18*2+20), 16);
-    return;
-  }
-
-  if (ch=='\n')
-  {
-    text_vb+=160;
-    cur_pos += 80;
-    set_cursor(cur_pos);
-    s32_print_int(cur_pos, (u8*)(0xb8000+160*1 + 18*2+40), 16);
-    return;
-  }
-  *text_vb = ch;
-  text_vb+=2;
-  set_cursor(++cur_pos);
-  s32_print_int(cur_pos, (u8*)(0xb8000+160*1 + 18*2+60), 16);
-}
-
-void s32_set_text_color(u8 fg, u8 bg)
-{
-  text_fg=fg;
-  text_bg=bg;
-}
 
 // prefix s32 means simple, 32bit code (in x86 protected mode)
 void s32_put_char(u8 ch, u8 *vb)
--- tty/console.c
+++ tty/console.c
@@ -2,6 +2,14 @@
 #include "type.h"
 #include "vga.h"
 #include "io.h"
+#include "console.h"
+#include "k_stdio.h"
+
+u8 *text_vb = (u8*)0xb8000;
+u8 text_fg;
+u8 text_bg;
+u16 cur_pos=0;
+
 
 void set_cursor(u16 pos)
 {
@@ -22,4 +30,36 @@ void set_video_start_addr(u16 addr)
   io_out8(CRTC_ADDR, START_ADDR_L);
   io_out8(CRTC_DATA, addr & 0xff);
   enable_int();
+}
+
+// prefix s32 means simple, 32bit code (in x86 protected mode)
+void s32_print_char(u8 ch)
+{
+  if (ch=='\r')
+  {
+    text_vb = (u8*)((u32)text_vb - (((u32)text_vb-0xb8000) % 160));
+    cur_pos = (((u32)text_vb - 0xb8000)/2);
+    set_cursor(cur_pos);
+    s32_print_int(cur_pos, (u8*)(0xb8000+160*1 + 18*2+20), 16);
+    return;
+  }
+
+  if (ch=='\n')
+  {
+    text_vb+=160;
+    cur_pos += 80;
+    set_cursor(cur_pos);
+    s32_print_int(cur_pos, (u8*)(0xb8000+160*1 + 18*2+40), 16);
+    return;
+  }
+  *text_vb = ch;
+  text_vb+=2;
+  set_cursor(++cur_pos);
+  s32_print_int(cur_pos, (u8*)(0xb8000+160*1 + 18*2+60), 16);
+}
+
+void s32_set_text_color(u8 fg, u8 bg)
+{
+  text_fg=fg;
+  text_bg=bg;
 }
