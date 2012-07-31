24262527597b065e036c385b997c1dd9db18dd2f
descent <descent@weiya.com.tw>
2012-07-31 11:48:11 +0800

    add draw_str() for testing draw text

--- vga/draw_func.c
+++ vga/draw_func.c
@@ -1,5 +1,6 @@
 #include "draw_func.h"
 #include "type.h"
+#include "font_rawdata.h"
 
 void draw_box()
 {
@@ -21,5 +22,41 @@ void draw_box_1(int draw_x, int draw_y, int c)
     {
       *(vb + x + 320*y) = c;
     }
+  void draw_str();
+  draw_str();
 }
 
+// current only for 320X240
+void draw_str()
+{
+  u8 *vb = (u8*)0xa0000;
+  u8 line=1;
+
+  const u8* image_addr = str;
+  int cur_x=0;
+
+  for (int y = 0 ; y < hb ; ++y)
+  {    
+    for (int x = 0 ; x < wb ; ++x)
+    { 
+      u8 c = *image_addr;
+          
+
+      for (int i=7 ; i>=0 ; --i)
+      {
+        if (((c >> i) & 0x1) == 1)
+          *vb = 60;
+        else
+          ;//printf("|");
+        ++cur_x;
+        ++vb;
+      }
+
+      ++image_addr;
+    }
+    cur_x = 0;
+   vb = (u8*)(0xa0000+(320*line));
+    ++line;
+  }
+
+}
