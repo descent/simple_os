3d23ee9e334c5d32a7fbd2e70c0126049ae22898
descent <descent@weiya.com.tw>
2012-08-03 17:03:33 +0800

    modified:   tty/tty.c     add select_tty()     modify init_tty().

--- tty/tty.c
+++ tty/tty.c
@@ -2,15 +2,49 @@
 #include "k_stdio.h"
 #include "console.h"
 
-Tty tty_table[TTY_NUM];
+static Tty tty_table[TTY_NUM];
 
-void init_tty(Tty *tty)
+static int cur_tty_index = 0;
+
+int select_tty(int tty_index)
+{ 
+  int ret =0;
+  
+  if ( 0 <= cur_tty_index && cur_tty_index < TTY_NUM)
+    cur_tty_index = tty_index;
+  else
+    ret = -1;
+  set_video_start_addr((tty_table[cur_tty_index].console->vm_start-0xb8000)/2);
+  //s32_print_int(tty_table[cur_tty_index].console->vm_start, (u8*)(0xb8000+160*1), 16);
+
+  switch (cur_tty_index)
+  {
+    case 0:
+      s32_console_print_char(tty_table[cur_tty_index].console, '0');
+      break;
+    case 1:
+      s32_console_print_char(tty_table[cur_tty_index].console, '1');
+      break;
+    case 2:
+      s32_console_print_char(tty_table[cur_tty_index].console, '2');
+      break;
+    default:
+      break;
+
+  }
+  return ret;
+}
+
+int init_tty(Tty *tty)
 {
   tty->inbuf_count = 0;
   tty->inbuf_head = tty->inbuf_tail = tty->inbuf;
 
   u8 tty_index = tty - tty_table;
   tty->console = console_table + tty_index;
+
+  init_console();
+  return 0;
 }
 
 void task_tty(void)
