9fb402029293ed585057c0fabbd17ecd14ffbfd8
descent <descent0@gmail.com>
2012-08-06 22:13:34 +0800

    modified:   tty/tty.c     fixed is_current_tty()     select_tty() remove debug message.     add virtual console (current 3 console).

--- tty/tty.c
+++ tty/tty.c
@@ -1,6 +1,10 @@
 #include "tty.h"
 #include "k_stdio.h"
 #include "console.h"
+#include "keyboard.h"
+
+#define TRUE 1
+#define FALSE (!TRUE)
 
 static Tty tty_table[TTY_NUM];
 
@@ -20,13 +24,13 @@ int select_tty(int tty_index)
   switch (cur_tty_index)
   {
     case 0:
-      s32_console_print_char(tty_table[cur_tty_index].console, '0');
+      //s32_console_print_char(tty_table[cur_tty_index].console, '0');
       break;
     case 1:
-      s32_console_print_char(tty_table[cur_tty_index].console, '1');
+      //s32_console_print_char(tty_table[cur_tty_index].console, '1');
       break;
     case 2:
-      s32_console_print_char(tty_table[cur_tty_index].console, '2');
+      //s32_console_print_char(tty_table[cur_tty_index].console, '2');
       break;
     default:
       break;
@@ -43,14 +47,87 @@ int init_tty(Tty *tty)
   u8 tty_index = tty - tty_table;
   tty->console = console_table + tty_index;
 
-  init_console();
   return 0;
+}
+
+int is_current_tty(Tty *tty)
+{
+  return (tty == &tty_table[cur_tty_index]);
+#if 0
+  if (cur_tty_index == (tty - tty_table)/sizeof(Tty))
+    return TRUE;
+  else
+    return FALSE;
+#endif
+}
+
+int get_data_from_tty_buf(Tty *tty)
+{
+  if (tty->inbuf_count)
+  {
+    u8 ch = *(tty->inbuf_tail);
+    if (tty->inbuf_tail == tty->inbuf + TTY_IN_BYTES_NUM)
+      tty->inbuf_tail = tty->inbuf;
+    --tty->inbuf_count;
+    return ch;
+  }
+  else
+    return -1;
+}
+
+int parse_key(Tty *tty, KeyStatus *key_status)
+{
+  static u8 keymap_col=0;
+
+  int scan_code = get_data_from_tty_buf(tty);
+  switch (scan_code)
+  {
+    case -1:
+      break;
+    case 0xe1:
+      break;
+    case 0xe0:
+      scan_code = get_data_from_tty_buf(tty);
+      key_status->key = lookup_key(scan_code & 0x7f, keymap_col);
+      key_status->press = ((scan_code & 0x80) ? RELEASE: PRESS);
+      break;
+    default:
+      key_status->key = lookup_key(scan_code & 0x7f, keymap_col);
+      key_status->press = ((scan_code & 0x80) ? RELEASE: PRESS);
+      break;
+  }
+}
+
+void tty_do_read(Tty *tty)
+{
+  if(is_current_tty(tty))
+  {
+    keyboard_read(tty); // call put_key
+    //KeyStatus key_status;
+
+    //parse_key(tty, &key_status);
+  }
+}
+
+void tty_do_write(Tty *tty)
+{
+  if (tty->inbuf_count)
+  {
+    u8 ch = *(tty->inbuf_tail);
+    ++(tty->inbuf_tail);
+    if (tty->inbuf_tail == tty->inbuf + TTY_IN_BYTES_NUM)
+      tty->inbuf_tail = tty->inbuf;
+    --tty->inbuf_count;
+    //s32_console_print_char(tty->console, ch);
+    s32_console_print_char(tty_table[cur_tty_index].console, ch);
+  }
 }
 
 void task_tty(void)
 {
   Tty *tty;
 
+  init_console();
   for (tty = tty_table ; tty < tty_table + TTY_NUM ; ++tty)
   {
     init_tty(tty);
@@ -61,9 +138,23 @@ void task_tty(void)
   {
     s32_print_int(i, (u8*)(0xb8000+160*23), 10);
     ++i;
+    for (tty = tty_table ; tty < tty_table + TTY_NUM ; ++tty)
+    {
+      tty_do_read(tty);
+      tty_do_write(tty);
+    }
   }
 }
 
-void tty_do_read(Tty *tty)
+int put_key(Tty *tty, u32 key)
 {
+  if (tty->inbuf_count < TTY_IN_BYTES_NUM)
+  {
+    s32_put_char(key, (u8*)(0xb8000+160*20));
+    *(tty->inbuf_head) = key;
+    ++tty->inbuf_head;
+    if (tty->inbuf_head == tty->inbuf+TTY_IN_BYTES_NUM)
+      tty->inbuf_head = tty->inbuf;
+    ++tty->inbuf_count;
+  }
 }
