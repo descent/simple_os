49fa3dc44aad33ce8e90d59ebef8f956ed50e107
descent <descent@weiya.com.tw>
2012-08-14 10:47:24 +0800

    rm useless code for more clear

--- cpp_runtime/global_object/dos_cpp/cppb.cpp
+++ cpp_runtime/global_object/dos_cpp/cppb.cpp
@@ -11,51 +11,16 @@ __asm__(".code16gcc\n");
 #define BOCHS_MB __asm__ __volatile__("xchg %bx, %bx");
 
 
-Io io;
 Ab ab(7);
+Io io;
 
 extern int _start_ctors;
 extern int _end_ctors;
 
-extern "C" void WinMain(void)
+extern "C" int cpp_main(void)
 {
+  //BOCHS_MB  
 
-  BOCHS_MB  
-  #if 1
-  //__asm__ ("mov  %cs, %ax\n");
-  //__asm__ ("mov  %ax, %ds\n");
-  //__asm__ ("mov  %ax, %ss\n");
-
-#if 0
-  int ctor_addr_start = _start_ctors;
-  int ctor_addr_end = _end_ctors;
-    typedef void (*FuncPtr)();
-    FuncPtr fp = (FuncPtr)(ctor_addr_start);
-    fp();
-#endif
-
-/* Test for GCC > 3.2.0 
- * ref: http://gcc.gnu.org/onlinedocs/gcc-3.4.6/cpp/Common-Predefined-Macros.html
-  #if __GNUC__ > 3 || \
-  (__GNUC__ == 3 && (__GNUC_MINOR__ > 2 || \
-  (__GNUC_MINOR__ == 2 && \
-  __GNUC_PATCHLEVEL__ > 0))
-*/
-
-  #if __GNUC__ >= 4 && __GNUC_MINOR__ <= 4
-  extern void _GLOBAL__I_io();
-  _GLOBAL__I_io();
-  #else // for 4.7
-  extern void _GLOBAL__sub_I_io();
-  _GLOBAL__sub_I_io();
-  #endif
-    #if 0
-  for (int i = ctor_addr_start; i < ctor_addr_end ; ++i)
-  {
-  }
-
-#endif
-  //__asm__ ("mov  $0xfff0, %sp\n");
   {
   const char *ver=__VERSION__;
   io.print("hello cpp class\r\n");
@@ -63,34 +28,15 @@ extern "C" void WinMain(void)
   io.print(ver);
   io.print("\r\n");
   }
-  #if 0
-  unsigned char *vb = (unsigned char *)0xb8000;
-  *vb = 'A';
-  *(unsigned char *)0xb8001 = 0xc;
-  *(unsigned char *)0xb8002 = 'B';
-  *(unsigned char *)0xb8003 = 0x9;
-  *(unsigned char *)0xb8004 = '@';
-  *(unsigned char *)0xb8005 = 0xc;
-  #endif
-  //while(1);
-  __asm__ ("mov     $0x4c00, %ax\n");
-  __asm__ ("int     $0x21\n"); //   回到 DOS
-
-  #endif
+  return 0;
 }
-
-
 
 void *__dso_handle;
 extern "C"
 {
 int __cxa_atexit(void (*destructor) (void *), void *arg, void *__dso_handle)
 {
-  io.print("__cxa_atexit\r\n");
+  io.print("__cxa_atexit: register dtor here\r\n");
   return 0;
-}
-void __cxa_finalize(void *d)
-{
-  io.print("hello dtor\r\n");
 }
 }
