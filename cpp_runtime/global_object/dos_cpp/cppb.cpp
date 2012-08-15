65d03e51927b592a6d967cbb426dab6a81a3ac63
descent <descent@weiya.com.tw>
2012-08-15 14:02:44 +0800

    add some debug message

--- cpp_runtime/global_object/dos_cpp/cppb.cpp
+++ cpp_runtime/global_object/dos_cpp/cppb.cpp
@@ -27,8 +27,48 @@ Io io;
 extern int _start_ctors;
 extern int _end_ctors;
 
+void s16_print_int(int i, int radix);
+void print_str(const char   *s);
+
+extern int obj_count;
+
+u16 asm_get_ds(void)
+{
+  u16 v=0;
+  __asm__ __volatile__
+    (
+      "mov %%ds, %%ax\n"
+      : "=a"(v)// output
+    );
+  return v;
+}
+
+u16 asm_get_cs(void)
+{
+  u16 v=0;
+  __asm__ __volatile__
+    (
+      "mov %%cs, %%ax\n"
+      : "=a"(v)// output
+    );
+  return v;
+}
+
+
 extern "C" int cpp_main(void)
 {
+  print_str("cpp_main\r\n");
+  s16_print_int(obj_count, 10);
+  print_str("\r\n");
+
+  u16 cs = asm_get_cs();
+  u16 ds = asm_get_ds();
+  io.print("cs: ");
+  s16_print_int(cs, 16);
+  io.print("\r\n");
+  io.print("ds: ");
+  s16_print_int(ds, 16);
+  io.print("\r\n");
   //BOCHS_MB  
   const char *ver=__VERSION__;
   io.print("io hello cpp class\r\n");
