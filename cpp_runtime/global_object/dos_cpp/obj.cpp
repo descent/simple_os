cd2d2a9bf5dca12d8ce3ddaef63507ba86c74eda
descent <descent@weiya.com.tw>
2012-08-17 09:03:56 +0800

    new file:   obj.cpp new file:   obj.h   for test

---
+++ cpp_runtime/global_object/dos_cpp/obj.cpp
@@ -0,0 +1,7 @@
+#include "obj.h"
+#include "dos_io.h"
+
+void Obj1::test_obj1()
+{
+  s16_print_int(255, 10);
+}
---
+++ cpp_runtime/global_object/dos_cpp/obj.h
@@ -0,0 +1,14 @@
+__asm__(".code16gcc\n");
+
+#ifndef OBJ_H
+#define OBJ_H
+
+class Obj1
+{
+  public:
+    void test_obj1();
+  private:
+    int data_member1_;
+};
+
+#endif
