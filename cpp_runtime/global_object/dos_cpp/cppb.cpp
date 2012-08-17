17564a13693823299ecbf881d31f08a6e447106b
descent <descent@weiya.com.tw>
2012-08-17 09:05:01 +0800

    move print io to dos_io.cpp

--- cpp_runtime/global_object/dos_cpp/cppb.cpp
+++ cpp_runtime/global_object/dos_cpp/cppb.cpp
@@ -1,6 +1,6 @@
 __asm__(".code16gcc\n");
 #include "io.h"
-
+#include "obj.h"
 
 typedef signed char s8;
 typedef signed short s16;
@@ -23,6 +23,7 @@ typedef unsigned int u32;
 
 Ab ab(7);
 Io io;
+Obj1 g_obj1;
 
 extern int _start_ctors;
 extern int _end_ctors;
@@ -75,61 +76,12 @@ extern "C" int cpp_main(void)
   io.print("io g++ version: ");
   io.print(ver);
   io.print("\r\n");
+
+  Obj1 obj1;
+  Obj1 obj2;
+  obj1 = obj2;
+  obj1.test_obj1();
+
   return 0;
 }
 
-void print_str(const char   *s)
-{
-  while(*s)
-  {
-    __asm__ __volatile__ ("int  $0x10" : : "a"(0x0E00 | *s), "b"(7));
-    s++;
-  }
-}
-
-char* itoa(int n, char* str, int radix)
-{
-  char digit[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char* p=str;
-  char* head=str;
-  //int radix = 10;
-
-//  if(!p || radix < 2 || radix > 36)
-//    return p;
-  if (n==0)
-  {
-    *p++='0';
-    *p=0;
-    return str;
-  }
-  if (radix == 10 && n < 0)
-  {
-    *p++='-';
-    n= -n;
-  }
-  while(n)
-  {
-    *p++=digit[n%radix];
-    n/=radix;
-  }
-  *p=0;
-  for (--p; head < p ; ++head, --p)
-  {
-    char temp=*head;
-    if (*(p-1) != '-')
-    {
-      *head=*p;
-      *p=temp;
-    }
-  }
-  return str;
-}
-
-void s16_print_int(int i, int radix)
-{
-  char str[12]="";
-  char *str_ptr = str;
-
-  str_ptr = itoa(i, str_ptr, radix);
-  print_str(str_ptr);
-}
