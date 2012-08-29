5fa08df042d7404a4eb147835c0a048052c3ebbf
descent <descent@weiya.com.tw>
2012-08-29 16:29:44 +0800

    modified:   c_init.S modified:   cb.c     print 0:0 address data, and use char *p=0 modify it value.

--- c_runtime/cb.c
+++ c_runtime/cb.c
@@ -4,8 +4,14 @@ void func(char c, int i, const char *ptr);
 
 void c_main(void)
 {
+  __asm__ ("mov $0, %ax");
+  __asm__ ("mov %ax, %ss");
+  __asm__ ("mov %ax, %ds");
   char *p=0;
   *p='a';
+  __asm__ ("mov %cs, %ax");
+  __asm__ ("mov %ax, %ss");
+  __asm__ ("mov %ax, %ds");
   //while(1);
 }
 
