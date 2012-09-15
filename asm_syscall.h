23a50f9da8033e00263fcccd4f09533bb1e005ee
descent <descent0@gmail.com>
2012-09-15 20:21:01 +0800

    new file:   asm_syscall.h

---
+++ asm_syscall.h
@@ -0,0 +1,14 @@
+#ifndef ASM_SYSCALL_H
+#define ASM_SYSCALL_H
+
+#define INT_VECTOR_SYS_CALL 0x90
+#define _NR_GET_TICKS 0
+#define _NR_SET_VGA_MODE 1
+#define _NR_WRITE 2
+
+#define NR_SYS_CALL 3
+
+//int get_ticks(void);
+//int set_vga_mode(void);
+
+#endif
