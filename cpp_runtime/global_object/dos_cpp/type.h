68191b2311d900492397c2456ea0b0fbd9b66e8f
descent <descent@weiya.com.tw>
2012-09-17 14:20:07 +0800

    new file:   type.h

---
+++ cpp_runtime/global_object/dos_cpp/type.h
@@ -0,0 +1,16 @@
+#ifndef TYPE_H
+#define TYPE_H
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+typedef void (*IntHandler)();
+
+typedef void (*IrqHandler)();
+
+#endif
+
