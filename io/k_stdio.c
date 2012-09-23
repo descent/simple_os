cc8ecf239bc5e9dba594a5049a93b83769d3df71
descent <descent0@gmail.com>
2012-09-23 17:01:55 +0800

    modified:   k_stdio.c     for kernel and kernel loader io function.

--- io/k_stdio.c
+++ io/k_stdio.c
@@ -1,3 +1,7 @@
+/*
+ * kernel and kernel loader share io function
+ */
+
 #include "type.h"
 #include "k_stdio.h"
 #include "console.h"
@@ -168,63 +172,3 @@ void p_dump_u8(u8 *buff, int len)
     #endif
 }
 
-int s32_printf(const char *fmt, ...)
-{
-  char buf[256];
-  int i=1;
-  char *p = buf;
-
-  for (; *fmt ; ++fmt)
-  {
-    if (*fmt != '%')
-    {
-      *p++ = *fmt;
-      continue;
-    }
-    ++fmt;
-    u8 *arg = (u8 *)(&fmt + i); // nst argument address
-    u32 arg_content = *((u32*)arg);
-
-    switch (*fmt)
-    {
-      case 'd':
-      {
-        u8 str[12]="";
-        s32_itoa(arg_content, str, 10);
-
-        char *str_ptr = str;
-        while(*str_ptr)
-          *p++ = *str_ptr++;
-        break;
-      }
-      case 'x':
-      {
-        u8 str[12]="";
-        s32_itoa(arg_content, str, 16);
-
-        char *str_ptr = str;
-        while(*str_ptr)
-          *p++ = *str_ptr++;
-        break;
-      }
-      case 's':
-      {
-        char *str_ptr = (char *)arg_content;
-        while(*str_ptr)
-          *p++ = *str_ptr++;
-        break;
-      }
-      default:
-        break;
-    }
-    ++i; // point to next argument
- 
-  } // end for (char *p = buf; *fmt ; ++fmt, ++i)
-  int len = p-buf;
-  //buf[len]='\0';
-
-  int write(char *buf, int len);
-  write(buf, len);
-
-  return 0;
-}
