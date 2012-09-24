79dc760fd0a431efc3093d6d7ad376151756a387
descent <descent@weiya.com.tw>
2012-09-24 08:35:26 +0800

    modified:   tool.cpp modified:   tool.h     use get_vb() to return u8 *vb; this can guarantee global object     constructor order.

--- cpp_runtime/pc/tool.cpp
+++ cpp_runtime/pc/tool.cpp
@@ -1,10 +1,21 @@
 #include "tool.h"
-#include "type.h"
 
 extern u32 load_addr;
-u8 *vb = (u8*)(0xb8000+160 - load_addr);
+
+u8 *get_vb()
+{
+  static u8 *vb = (u8*)(0xb8000+160 - load_addr);  
+  return vb;
+}
+
 void print(char c)
 {
+//  void _GLOBAL__I_vb();
+//  _GLOBAL__I_vb();
+  //vb -= load_addr;
+  BOCHS_MB
+
+  u8 *vb = get_vb();
   *vb++ = c;
   *vb++ = 9;
 }
--- cpp_runtime/pc/tool.h
+++ cpp_runtime/pc/tool.h
@@ -1,9 +1,12 @@
 #ifndef TOOL_H
 #define TOOL_H
 
+#include "type.h"
+
 #define BOCHS_MB __asm__ __volatile__("xchg %bx, %bx");
 
 void print(char c);
 void print(const char *s);
+u8 *get_vb();
 
 #endif
