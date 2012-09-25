be2f01b4b165e3ea0f23d2c6cf6163ba740d8000
descent <descent@weiya.com.tw>
2012-09-25 10:30:25 +0800

    new file:   font_rawdata.h     declare fonr_rawdata.c variable.

---
+++ vga/font_rawdata.h
@@ -0,0 +1,12 @@
+#ifndef FONT_RAWDATA_H
+#define FONT_RAWDATA_H
+#include "type.h"
+
+extern u8 str[];
+extern int wb;
+extern int hb;
+extern u8 ch_data[];
+extern int ch_wb;
+extern int ch_hb;
+
+#endif
