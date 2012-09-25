fb73893a5c578542ce6786949a344ebb6a3b728d
descent <descent@weiya.com.tw>
2012-09-25 14:06:17 +0800

    use g_vb to modify video address

--- cpp_runtime/pc/tool.cpp
+++ cpp_runtime/pc/tool.cpp
@@ -2,10 +2,18 @@
 
 extern u32 load_addr;
 
+u8 *g_vb = 0;
+
 u8 *get_vb()
 {
   static u8 *vb = (u8*)(0xb8000+160 - load_addr);  
-  return vb;
+  if (g_vb == 0)
+  {
+    g_vb = vb;
+    return vb;
+  }
+  else
+    return g_vb;
 }
 
 void print(char c)
@@ -18,6 +26,7 @@ void print(char c)
   u8 *vb = get_vb();
   *vb++ = c;
   *vb++ = 9;
+  g_vb = vb;
 }
 
 void print(const char *s)
