685c178245b9e849f43460fcd8314bb33a983be2
descent <descent@weiya.com.tw>
2012-09-27 11:30:30 +0800

    add s_strcpy()

--- include/k_string.h
+++ include/k_string.h
@@ -8,7 +8,7 @@ void asm_memcpy(u8 *dest, u8 *src, int n);
 void asm_seg_memcpy(u8 *dest, u8 *src, int n, u16 fs);
 void asm_absolute_memcpy(u8 *dest, u8 *src, int n);
 
-// prefix s is simple
+// prefix s is simple; 32 means in protoected mode
 static inline u32 s_strlen(const char *s)
 {
   u8 len=0;
@@ -16,6 +16,15 @@ static inline u32 s_strlen(const char *s)
   while(*s++)
     ++len;
   return len;
+}
+
+static inline char* s_strcpy(char *dest, const char *src)
+{
+  char *p = dest;
+  while(*dest)
+    *dest++ = *src++;
+  *dest='\0';
+  return p;
 }
 
 static inline int s_strcmp(const char *s1, const char *s2)
