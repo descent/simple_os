9eaac001935d3acd90c2e98c9121edd81ce93e9c
descent <descent@weiya.com.tw>
2012-09-27 11:29:15 +0800

    add s32_sprintf(), s32_printk()

--- io/k_stdio_k.c
+++ io/k_stdio_k.c
@@ -7,6 +7,62 @@
 #include "type.h"
 #include "k_stdlib.h"
 
+int s32_sprintf(char *str_buf, const char *fmt, ...)
+{
+  //char buf[256];
+  int i=1;
+  char *p = str_buf;
+
+  for (; *fmt ; ++fmt)
+  {
+    if (*fmt != '%')
+    {
+      *p++ = *fmt;
+      continue;
+    }
+    ++fmt;
+    u8 *arg = (u8 *)(&fmt + i); // nst argument address
+    u32 arg_content = *((u32*)arg);
+
+    switch (*fmt)
+    {
+      case 'd':
+      {
+        u8 str[12]="";
+        s32_itoa(arg_content, str, 10);
+
+        char *str_ptr = str;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      case 'x':
+      {
+        u8 str[12]="";
+        s32_itoa(arg_content, str, 16);
+
+        char *str_ptr = str;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      case 's':
+      {
+        char *str_ptr = (char *)arg_content;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      default:
+        break;
+    }
+    ++i; // point to next argument
+ 
+  } // end for (char *p = buf; *fmt ; ++fmt, ++i)
+  *p='\0';
+  return p-str_buf - 1;
+}
+
 int s32_printf(const char *fmt, ...)
 {
   char buf[256];
@@ -59,11 +115,15 @@ int s32_printf(const char *fmt, ...)
     ++i; // point to next argument
  
   } // end for (char *p = buf; *fmt ; ++fmt, ++i)
-  int len = p-buf;
+  int len = p-buf - 1;
   //buf[len]='\0';
 
   int write(char *buf, int len);
   write(buf, len);
 
   return 0;
+}
+
+int s32_printk(const char *fmt, ...)
+{
 }
