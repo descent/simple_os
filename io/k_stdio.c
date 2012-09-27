def8cb16dcda001fdc26dde3a65938bb8b0222f8
root <root@weiya-linux.(none)>
2012-09-27 16:14:44 +0800

    add s32_sprintf(), s32_vsprintf()

--- io/k_stdio.c
+++ io/k_stdio.c
@@ -199,3 +199,111 @@ void h2c(u8 hex, u8 ch[2])
   }
 }
 
+int s32_vsprintf(char *str_buf, const char *fmt, char * ap)
+{
+  char *p = str_buf;
+  u8 *arg = ap;
+
+  for (; *fmt ; ++fmt)
+  {
+    if (*fmt != '%')
+    {
+      *p++ = *fmt;
+      continue;
+    }
+    ++fmt;
+    u32 arg_content = *((u32*)arg);
+
+    switch (*fmt)
+    {
+      case 'd':
+      {
+        u8 str[12]="";
+        s32_itoa(arg_content, str, 10);
+
+        char *str_ptr = str;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      case 'x':
+      {
+        u8 str[12]="";
+        s32_itoa(arg_content, str, 16);
+
+        char *str_ptr = str;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      case 's':
+      {
+        char *str_ptr = (char *)arg_content;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      default:
+        break;
+    }
+    arg+=4;
+ 
+  } // end for (char *p = buf; *fmt ; ++fmt, ++i)
+  *p='\0';
+  return p-str_buf - 1;
+}
+
+int s32_sprintf(char *str_buf, const char *fmt, ...)
+{
+  int i=1;
+  char *p = str_buf;
+
+  for (; *fmt ; ++fmt)
+  {
+    if (*fmt != '%')
+    {
+      *p++ = *fmt;
+      continue;
+    }
+    ++fmt;
+    u8 *arg = (u8 *)(&fmt + i); // nst argument address
+    u32 arg_content = *((u32*)arg);
+
+    switch (*fmt)
+    {
+      case 'd':
+      {
+        u8 str[12]="";
+        s32_itoa(arg_content, str, 10);
+
+        char *str_ptr = str;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      case 'x':
+      {
+        u8 str[12]="";
+        s32_itoa(arg_content, str, 16);
+
+        char *str_ptr = str;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      case 's':
+      {
+        char *str_ptr = (char *)arg_content;
+        while(*str_ptr)
+          *p++ = *str_ptr++;
+        break;
+      }
+      default:
+        break;
+    }
+    ++i; // point to next argument
+ 
+  } // end for (char *p = buf; *fmt ; ++fmt, ++i)
+  *p='\0';
+  return p-str_buf - 1;
+}
