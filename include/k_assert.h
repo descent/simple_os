04142f3f1e1a88bfc8e0e3cb4ad5230df7ccd029
descent <descent@weiya.com.tw>
2012-10-04 10:55:10 +0800

    complete panic(), assertion_failure(), but has bug

--- include/k_assert.h
+++ include/k_assert.h
@@ -2,6 +2,7 @@
 #define K_ASSERT_H
 
 #include "k_stdio.h"
+//#include "syscall.h"
 
 void assertion_failure(char *exp, char *file, char *base_file, int line);
 #define assert(exp) if (exp) ;\
@@ -11,7 +12,7 @@ void panic(const char *fmt, ...);
 
 static inline void spin(char *func_name)
 {
-  s32_printk("\nspinning in %s ...\n", func_name);
+  printx("\nspinning in %s ...\n", func_name);
   while(1);
 }
 
--- k_assert.c
+++ k_assert.c
@@ -2,14 +2,22 @@
 
 void assertion_failure(char *exp, char *file, char *base_file, int line)
 {
-  s32_printk("");
+  printx("%c asert(%s) failed: file: %s, base_file: %s, ln%d", MAG_CH_ASSERT, exp, file, base_file, line);
 
   spin("assertion_failure()");
-
   __asm__ __volatile__("ud2");
 }
 
 
 void panic(const char *fmt, ...)
 {
+  int i;
+  char buf[256];
+
+  char *arg = (char *)(&fmt+1);
+  int len=s32_vsprintf(buf, fmt, arg);
+
+  printx("%c !!panic!! %s", MAG_CH_PANIC, buf);
+
+  __asm__ __volatile__("ud2");
 }
