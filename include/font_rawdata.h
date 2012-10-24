31b6e94c5371c03b00dbd74f0f6ca48a8d2522e2
descent <descent@weiya.com.tw>
2012-10-24 08:37:58 +0800

    modified:   include/font_rawdata.h modified:   vga/font_rawdata.c     add 320X180X256 bmp raw data.

--- include/font_rawdata.h
+++ include/font_rawdata.h
@@ -9,4 +9,8 @@ extern u8 ch_data[];
 extern int ch_wb;
 extern int ch_hb;
 
+extern int pe_320_180_256_wb;
+extern int pe_320_180_256_hb;
+extern u8 pe_320_180_256_bmp[];
+extern u8 pe_320_180_256_palette_data[];
 #endif
