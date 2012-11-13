d289bc33a5bb2042a4c57d5a6ef03e6d77bfcda7
descent <descent0@gmail.com>
2012-11-13 21:59:10 +0800

    print echo

--- apps/echo.c
+++ apps/echo.c
@@ -1,6 +1,10 @@
 
 int main(int argc, const char *argv[])
 {
-        
+  *((unsigned char *)(0xb8000+160*22+0)) = 'e';
+  *((unsigned char *)(0xb8000+160*22+2)) = 'c';
+  *((unsigned char *)(0xb8000+160*22+4)) = 'h';
+  *((unsigned char *)(0xb8000+160*22+6)) = 'o';
+  while(1);
   return 0;
 }
