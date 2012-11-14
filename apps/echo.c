24d3bb38636afd83de57e9e3c064524d7240302c
descent <descent@weiya.com.tw>
2012-11-14 16:05:43 +0800

    add color

--- apps/echo.c
+++ apps/echo.c
@@ -2,9 +2,13 @@
 int main(int argc, const char *argv[])
 {
   *((unsigned char *)(0xb8000+160*22+0)) = 'e';
+  *((unsigned char *)(0xb8000+160*22+1)) = 0x14;
   *((unsigned char *)(0xb8000+160*22+2)) = 'c';
+  *((unsigned char *)(0xb8000+160*22+3)) = 0x94;
   *((unsigned char *)(0xb8000+160*22+4)) = 'h';
+  *((unsigned char *)(0xb8000+160*22+5)) = 0x0f;
   *((unsigned char *)(0xb8000+160*22+6)) = 'o';
+  *((unsigned char *)(0xb8000+160*22+7)) = 0x0c;
   while(1);
   return 0;
 }
