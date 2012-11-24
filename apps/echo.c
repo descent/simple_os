d5cf45e04c09ac8901d2d0ab8fb4a208f6e64c5e
descent <descent0@gmail.com>
2012-11-24 23:57:28 +0800

    modified:   echo.c modified:   makefile modified:   start.S     modify for app using system call, but not yet complete.

--- apps/echo.c
+++ apps/echo.c
@@ -1,6 +1,7 @@
 
 int main(int argc, const char *argv[])
 {
+#if 0
   *((unsigned char *)(0xb8000+160*22+0)) = 'e';
   *((unsigned char *)(0xb8000+160*22+1)) = 0x14;
   *((unsigned char *)(0xb8000+160*22+2)) = 'c';
@@ -9,6 +10,7 @@ int main(int argc, const char *argv[])
   *((unsigned char *)(0xb8000+160*22+5)) = 0x0f;
   *((unsigned char *)(0xb8000+160*22+6)) = 'o';
   *((unsigned char *)(0xb8000+160*22+7)) = 0x0c;
+#endif
   while(1);
   return 0;
 }
