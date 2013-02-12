ec81a2d1d7fad2b426dff171e340c2f6c65a9e77
descent <descent0@gmail.com>
2013-02-12 17:46:29 +0800

    add LOAD_KERNEL_ADDR

--- include/kl_global.h
+++ include/kl_global.h
@@ -8,11 +8,12 @@
 #define BLOCK_SIZE  512
 #define READ_FAT_ADDR (0x3000) // original is 0x2000, but will overwrite bss (variable bpb), so change to 0x3000
 #define IMAGE_LMA   (0x4000)
-#define LOAD_KERNEL_OFFSET (0x0)
+//#define LOAD_KERNEL_OFFSET (0x0)
+#define LOAD_KERNEL_OFFSET (0x200000)
 //#define IMAGE_LMA   0x8000
 #define IMAGE_ENTRY 0x800c
 #define buf_addr_val (*(u8 volatile*(IMAGE_LMA)))
-
+#define LOAD_KERNEL_ADDR 0x300000 // in big real mode, copy buffer (load by int 0x13) to the address
 
 #endif
 
