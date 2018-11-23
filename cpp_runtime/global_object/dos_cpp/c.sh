6ff132934cab57a13ce5bcc0353dd25722d2731e
descent.sung <descent.sung@deltaww.com>
2018-11-23 13:02:31 +0800

    cp reloc.bin

--- cpp_runtime/global_object/dos_cpp/c.sh
+++ cpp_runtime/global_object/dos_cpp/c.sh
@@ -2,5 +2,6 @@
 sudo mount -o loop dos622.img  /floppy/
 sudo cp -v cppb.bin  /floppy/cppb.com
 sudo cp -v nullptr.bin  /floppy/nullptr.com
+sudo cp -v reloc.bin  /floppy/reloc.com
 sudo  umount /floppy 
 
